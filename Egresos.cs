5f013bb66ae2e0f25e5482700744e74f53f8dcae
Cesar Bernal <mrcode.hack28@gmail.com>
2023-03-14 15:59:55 -0700

    Atributos y encapsulamiento Egresos

--- Egresos.cs
+++ Egresos.cs
@@ -1,3 +1,43 @@
 public class Egresos {
-    
+    // Atributos
+    private string? nss;
+    private int cama;
+    private DateTime fechaIngreso;
+    private int estado;
+
+    // Constructores
+    public Egresos() {
+        this.nss = "default";
+        this.cama = 0;
+        this.fechaIngreso = DateTime.Now;
+        this.estado = 1;
+    }
+
+    public Egresos(string? nss, int cama, DateTime fechaIngreso, int estado) {
+        this.nss = nss;
+        this.cama = cama;
+        this.fechaIngreso = fechaIngreso;
+        this.estado = estado;
+    }
+
+    // Encapsulamiento
+    public string? NSS {
+        get {return this.nss;}
+        set {this.nss = value;}
+    }
+
+    public int Cama {
+        get {return this.cama;}
+        set {this.cama = value;}
+    }
+
+    public DateTime FechaIngreso {
+        get {return this.fechaIngreso;}
+        set {this.fechaIngreso = value;}
+    }
+
+    public int Estado {
+        get {return this.estado;}
+        set {this.estado = value;}
+    }
 }

8673dd5472cad969312fe275f527f39ef692d698
Cesar Bernal <mrcode.hack28@gmail.com>
2023-03-15 13:06:01 -0700

    Cambio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using src.Model;
+
+namespace GRDSystem {
+    class Program {
+        static void Main(string[] args) {
+            ModeloConexion conexion = new ModeloConexion();
+
+            if(conexion.establecerConexion() == true) {
+                Console.WriteLine("Conexion establecida con exito");
+            }
+            else {
+                Console.WriteLine("Fallo");
+            }
+        }
+    }
+}
--- src/View/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GRDSystem {
-    class Program {
-        static void Main(string[] args) {
-            
-        }
-    }
-}

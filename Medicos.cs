421290bbbfd4d6c6543760b1fda127f2e599e1f4
Cesar Bernal <mrcode.hack28@gmail.com>
2023-03-15 09:54:24 -0700

    Mejora encapsulamiento Medicos

--- Medicos.cs
+++ Medicos.cs
@@ -1,68 +1,32 @@
 public class Medicos {
     // Atributos
-    private int id;
-    private string? nombre;
-    private string? apellidoPaterno;
-    private string? apellidoMaterno;
-    private int edad;
-    private string? especialidad;
-    private int estado;
+    private int ID {get; set;}
+    private string? Nombre {get; set;}
+    private string? ApellidoPaterno {get; set;}
+    private string? ApellidoMaterno {get; set;}
+    private int Edad {get; set;}
+    private string? Especialidad {get; set;}
+    private int Estado {get; set;}
 
     // Constructores
     public Medicos() {
-        this.id = 0;
-        this.nombre = "default";
-        this.apellidoPaterno = "default";
-        this.apellidoMaterno = "default";
-        this.edad = 0;
-        this.especialidad = "default";
-        this.estado = 1;
+        this.ID = 0;
+        this.Nombre = "default";
+        this.ApellidoPaterno = "default";
+        this.ApellidoMaterno = "default";
+        this.Edad = 0;
+        this.Especialidad = "default";
+        this.Estado = 1;
     }
 
     public Medicos(int id, string? nombre, string? apellidoPaterno, string? apellidoMaterno,
                     int edad, string? especialidad, int estado) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellidoPaterno = apellidoPaterno;
-        this.apellidoMaterno = apellidoMaterno;
-        this.edad = edad;
-        this.especialidad = especialidad;
-        this.estado = estado;
-    }
-
-    // Encapsulamiento
-    public int ID {
-        get {return this.id;}
-        set {this.id = value;}
-    }
-
-    public string? Nombre {
-        get {return this.nombre;}
-        set {this.nombre = value;}
-    }
-
-    public string? ApellidoPaterno {
-        get {return this.apellidoPaterno;}
-        set {this.apellidoPaterno = value;}
-    }
-
-    public string? ApellidoMaterno {
-        get {return this.apellidoMaterno;}
-        set {this.apellidoMaterno = value;}
-    }
-
-    public int Edad {
-        get {return this.edad;}
-        set {this.edad = value;}
-    }
-
-    public string? Especialidad {
-        get {return this.especialidad;}
-        set {this.especialidad = value;}
-    }
-
-    public int Estado {
-        get {return this.estado;}
-        set {this.estado = value;}
+        this.ID = id;
+        this.Nombre = nombre;
+        this.ApellidoPaterno = apellidoPaterno;
+        this.ApellidoMaterno = apellidoMaterno;
+        this.Edad = edad;
+        this.Especialidad = especialidad;
+        this.Estado = estado;
     }
 }

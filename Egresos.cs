3f12aa6f5a5edaa5b6ae4f9ba5ab789030a9484e
Cesar Bernal <mrcode.hack28@gmail.com>
2023-03-15 09:56:00 -0700

    Mejora encapsulamiento Egresos

--- Egresos.cs
+++ Egresos.cs
@@ -1,43 +1,22 @@
 public class Egresos {
     // Atributos
-    private string? nss;
-    private int cama;
-    private DateTime fechaIngreso;
-    private int estado;
+    private string? NSS {get; set;}
+    private int Cama {get; set;}
+    private DateTime FechaIngreso {get; set;}
+    private int Estado {get; set;}
 
     // Constructores
     public Egresos() {
-        this.nss = "default";
-        this.cama = 0;
-        this.fechaIngreso = DateTime.Now;
-        this.estado = 1;
+        this.NSS = "default";
+        this.Cama = 0;
+        this.FechaIngreso = DateTime.Now;
+        this.Estado = 1;
     }
 
     public Egresos(string? nss, int cama, DateTime fechaIngreso, int estado) {
-        this.nss = nss;
-        this.cama = cama;
-        this.fechaIngreso = fechaIngreso;
-        this.estado = estado;
-    }
-
-    // Encapsulamiento
-    public string? NSS {
-        get {return this.nss;}
-        set {this.nss = value;}
-    }
-
-    public int Cama {
-        get {return this.cama;}
-        set {this.cama = value;}
-    }
-
-    public DateTime FechaIngreso {
-        get {return this.fechaIngreso;}
-        set {this.fechaIngreso = value;}
-    }
-
-    public int Estado {
-        get {return this.estado;}
-        set {this.estado = value;}
+        this.NSS = nss;
+        this.Cama = cama;
+        this.FechaIngreso = fechaIngreso;
+        this.Estado = estado;
     }
 }

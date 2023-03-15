d728cde6c4addb8b9ed3c9d1786911073b8d156f
Cesar Bernal <mrcode.hack28@gmail.com>
2023-03-15 09:51:26 -0700

    Mejora encapsulamiento

--- Pacientes.cs
+++ Pacientes.cs
@@ -1,93 +1,42 @@
 public class Paciente {
     // Atributos
-    private string? NSS;
-    private string? apellidoPaterno;
-    private string? apellidoMaterno;
-    private string? enfermedad;
-    private int medico;
-    private string? genero;
-    private int cama;
-    private DateTime fechaIngreso;
-    private DateTime fechaNacimiento;
-    private int estado;
+    private string? NSS {get; set;} 
+    private string? ApellidoPaterno {get; set;}
+    private string? ApellidoMaterno {get; set;}
+    private string? Enfermedad {get; set;}
+    private int Medico {get; set;}
+    private string? Genero {get; set;}
+    private int Cama {get; set;}
+    private DateTime FechaIngreso {get; set;}
+    private DateTime FechaNacimiento {get; set;}
+    private int Estado {get; set;}
 
     // Constructores
     public Paciente() {
         this.NSS = "default";
-        this.apellidoPaterno = "default";
-        this.apellidoMaterno = "default";
-        this.enfermedad = "defaul";
-        this.medico = 0;
-        this.genero = "default";
-        this.cama = 0;
-        this.fechaIngreso = DateTime.Now;
-        this.fechaNacimiento = DateTime.Now;
-        this.estado = 1;
+        this.ApellidoPaterno = "default";
+        this.ApellidoMaterno = "default";
+        this.Enfermedad = "defaul";
+        this.Medico = 0;
+        this.Genero = "default";
+        this.Cama = 0;
+        this.FechaIngreso = DateTime.Now;
+        this.FechaNacimiento = DateTime.Now;
+        this.Estado = 1;
     }
 
     public Paciente(string? nss, string? apellidoPaterno, string? apellidoMaterno,
                     string? enfermedad, int medico, string? genero, int cama,
                     DateTime fechaIngreso, DateTime fechaNacimiento, int estado) {
         this.NSS = nss;
-        this.apellidoPaterno = apellidoPaterno;
-        this.apellidoMaterno = apellidoMaterno;
-        this.enfermedad = enfermedad;
-        this.medico = medico;
-        this.genero = genero;
-        this.cama = cama;
-        this.fechaIngreso = fechaIngreso;
-        this.fechaNacimiento = fechaNacimiento;
-        this.estado = estado;
-    }
-
-    // Encapsulación
-    public string? Nss {
-        get {return this.NSS;}
-        set {this.NSS = value;}
-    }
-
-    public string? ApellidoPaterno {
-        get {return this.apellidoPaterno;}
-        set {this.apellidoPaterno = value;}
-    }
-
-     public string? ApellidoMaterno {
-        get {return this.apellidoMaterno;}
-        set {this.apellidoMaterno = value;}
-    }
-
-    public string? Enfermedad {
-        get {return this.enfermedad;}
-        set {this.enfermedad = value;}
-    }
-
-    public int Medico {
-        get {return this.medico;}
-        set {this.medico = value;}
-    }
-
-    public string? Genero {
-        get {return this.genero;}
-        set {this.genero = value;}
-    }
-
-    public int Cama {
-        get {return this.cama;}
-        set {this.cama = value;}
-    }
-
-    public DateTime FechaIngreso {
-        get {return this.fechaIngreso;}
-        set {this.fechaIngreso = value;}
-    }
-
-    public DateTime FechaNacimiento {
-        get {return this.fechaNacimiento;}
-        set {this.fechaNacimiento = value;}
-    }
-
-    public int Estado {
-        get {return this.estado;}
-        set {this.estado = value;}
+        this.ApellidoPaterno = apellidoPaterno;
+        this.ApellidoMaterno = apellidoMaterno;
+        this.Enfermedad = enfermedad;
+        this.Medico = medico;
+        this.Genero = genero;
+        this.Cama = cama;
+        this.FechaIngreso = fechaIngreso;
+        this.FechaNacimiento = fechaNacimiento;
+        this.Estado = estado;
     }
 }

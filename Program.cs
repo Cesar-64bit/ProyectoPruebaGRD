d92ef0e16570d06fd3bf3630ccb3f6a9e9becfc6
Cesar Bernal <mrcode.hack28@gmail.com>
2023-03-15 13:24:07 -0700

    Prueba ignorar

--- Program.cs
+++ Program.cs
@@ -3,14 +3,7 @@
 namespace GRDSystem {
     class Program {
         static void Main(string[] args) {
-            ModeloConexion conexion = new ModeloConexion();
-
-            if(conexion.establecerConexion() == true) {
-                Console.WriteLine("Conexion establecida con exito");
-            }
-            else {
-                Console.WriteLine("Fallo");
-            }
+            
         }
     }
 }
